ba684f552c3e74092e171058794e96eeb5ca725c
josé Roberto Hernández Alvarez <joser.hernandez@ciencias.unam.mx>
2019-11-10 12:33:02 -0600

    archivo de configuración

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+
+return
+[
+	//Configuración de la base de datos
+	
+	"db_host" => "", // Nombre del host de la base (generalmente es localhost).
+	"db_user" => "", // Usuario de la base de datos.
+	"db_name" => "", // Nombre de la base de datos.
+	"db_pass" => "", // Password.
+
+	
+	//Otras configuraciones
+
+	"base_dir" => "", // Ruta absoluta hacia el proyecto (C:\user...)(/var/www..).
+
+	// Ruta web hacia el proyecto (http://...)
+	// Para pruebas locales generalmente  es //localhost/<nombre de la carpeta 
+	// del proyecto>
+	"doc_root" => "//localhost" //
+]
+
+?>
